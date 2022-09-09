871c4ef5240a65b67ec6a16cfe3cd5e28dd926ad
god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>
2022-09-09 10:49:04 +0300

    Add files via upload

---
+++ html/footer.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Arvo&display=swap" rel="stylesheet">
+  <link href="..\assets\dtm.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:500,900,100,300,700,400' rel='stylesheet' type='text/css'>
+  <script src="..\assets\dtm.js"></script>
+  <script src="https://kit.fontawesome.com/9f86d2bf15.js" crossorigin="anonymous"></script>
+
+<body>
+    <footer class="text-center text-lg-start ">
+        <section class="">
+          <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto my-4">
+                <h6 class="text-uppercase fw-bold my-4">
+                 DTM Elektroteknik A.Ş.
+                </h6>
+                <p>
+                  <br><br><br><br><br><br>
+                  © 2022 Copyright: DTM Elektroteknik A.Ş.<br>
+                  <a class="text-reset " href="">KVKK Aydınlatma Metni</a><br>
+          <a class="text-reset " href="">KVKK Veri Sorumlusuna Başvuru Formu</a>
+                </p>
+              </div>
+              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto my-4">
+                <h6 class="text-uppercase fw-bold my-4">
+                  ÜRÜNLER
+                </h6>
+                <p>
+                  <a href="./products.html#product1" class="text-reset">AB-E AYDINLATMA BUSBAR <br>(25A-63A)</a>
+                </p>
+                <p>
+                  <a href="./products.html#product2" class="text-reset">SP-H ALÇAK GÜÇ BUSBAR <br>(100A-160A)</a>
+                </p>
+                <p>
+                  <a href="./products.html#product3" class="text-reset">MP-H ORTA GÜÇ BUSBAR <br>(160A-600A)</a>
+                </p>
+                <p>
+                  <a href="./products.html#product4" class="text-reset">HP-K YÜKSEK GÜÇ BUSBAR <br>(400A-6300A)</a>
+                </p>
+              </div>
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto my-4">
+                <h6 class="text-uppercase fw-bold my-4">
+                  Hızlı Navigasyon
+                </h6>
+                <p>
+                  <a href="iletisim.html" class="text-reset">İletişim</a>
+                </p>
+                <p>
+                  <a href="products.html" class="text-reset">Ürünler</a>
+                </p>
+                <p>
+                  <a href="mainpage.html" class="text-reset">Ana Sayfa</a>
+                </p>
+                <p>
+                  <a href="haberler.html" class="text-reset">Haberler</a>
+                </p>
+              </div>
+              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 my-4">
+                <h6 class="text-uppercase fw-bold my-4">İLETİŞİM</h6>
+                <p><i class="fas fa-home me-3"></i> Çatalmeşe Mah. Sultansuyu Cad. No:129, 34794 Alemdağ - Çekmeköy, İSTANBUL - TÜRKİYE</p>
+                <p>
+                  <i class="fas fa-envelope me-3"></i>
+                  info@dtmbusbar.com
+                </p>
+                <p><i class="fas fa-phone me-3"></i> 0216 429 51 74</p>
+                <p><i class="fas fa-print me-3"></i> 0216 429 47 88</p>
+                <div id="icons">
+                  <span>
+                  <a href="">
+                    <i class="fa-brands fa-twitter fa-xl"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa-brands fa-facebook fa-xl"></i>
+                  </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </footer>
+       
+</body>
