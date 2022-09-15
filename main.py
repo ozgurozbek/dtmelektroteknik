20e9fdd6fc4212bc11e8c21e4638e940001431b6
god-hephaestus <111083432+god-hephaestus@users.noreply.github.com>
2022-09-15 21:25:34 +0300

    backend
    backend patch 1

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template('mainpage.html')
+    
+@app.route("/kurumsal")
+def page_kurumsal():
+    return render_template('kurumsal.html')
+
+@app.route("/products")
+def page_products():
+    return render_template('products.html')
+
+@app.route("/hizmetler")
+def page_hizmetler():
+    return render_template('hizmetler.html')
+
+@app.route("/referanslar")
+def page_referanslar():
+    return render_template('referanslar.html')
+
+@app.route("/iletisim")
+def page_iletisim():
+    return render_template('iletisim.html')
+
+
+if __name__ == "__main__":
+    app.run()
